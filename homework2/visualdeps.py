852324acf50664062ba292ca4b727874580df6a5
theo-jedi <feds.msc@gmail.com>
2022-11-30 20:42:50 +0300

    Add homework2

---
+++ homework2/visualdeps.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class DependenciesVisualizer:
+
+    def fetch(self, package_name: str, tab_level: int = 1):
+        request = requests.get(f"https://www.npmjs.com/package/{package_name}/?activeTab=dependencies").content.decode()
+        soup = BeautifulSoup(request, features="lxml")
+
+        deps = soup.find("ul", {"aria-label": "Dependencies"})
+
+        if deps is None:
+            return
+
+        for dep in deps.find_all("a"):
+            dep = dep.text
+            print("\t" * tab_level, package_name, " -> ", dep, sep="")
+            self.fetch(dep, tab_level + 1)
+
+    def check(self, package_name: str):
+        print("Digraph {")
+        self.fetch(package_name)
+        print("}")
+
+
+if __name__ == '__main__':
+    visualizer = DependenciesVisualizer()
+    while True:
+        visualizer.check(input("Package name: "))
